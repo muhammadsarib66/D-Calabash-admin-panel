c58a6471c53bcfa9cee26b116117e4416a96d8e0
muhammadsarib66 <saribnoor0310@gmail.com>
2024-06-10 20:14:18 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import MiniDrawer from "./components/MiniDrawer";
 import Login from "./screens/Login";
 import { GetCustomerApi } from "./feature/slicer/GetCustomerListSlicer";
@@ -10,10 +10,18 @@ import { GetCategoriesListApi } from "./feature/slicer/GetCategoriesSlicer";
 import { GetRidersListApi } from "./feature/slicer/GetRidersSlicer";
 import { DashboardApi } from "./feature/slicer/DashboardSlicer";
 import { GetAdminListApi } from "./feature/slicer/GetAdminListingSlicer";
+import { io } from "socket.io-client";
+import { baseUrl } from "./feature/slicer/Slicer";
+import { toast } from "react-toastify";
 
 function App() {
+  // const MybaseUrl = "http://192.168.100.13:3000/"
+const socket = useMemo(() => io(baseUrl), []);
   const dispatch = useDispatch();
   const token = localStorage.getItem("admintoken");
+  const {_id}  = JSON.parse(localStorage.getItem('AdminUser') as string);
+  console.log(_id)
+  
   useEffect(() => {
     if (token) {
       dispatch(GetCustomerApi());
@@ -24,6 +32,15 @@ function App() {
       dispatch(DashboardApi());
       dispatch(GetAdminListApi());
     }
+    socket.emit('join-admin',_id);
+    socket.on("newOrder", (data) => {
+      toast.success("New Order Recieved"); 
+      console.log(data, "socketData recieved");
+    });
+
+
+    socket.on("disconnect", () => {
+    });
   }, [dispatch, token]);
   return (
     <>
